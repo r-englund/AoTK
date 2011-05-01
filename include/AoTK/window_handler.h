fe0afe2ee8f77a0d66adf5b61d1ae10278d24738
r-englund <rickard@r-englund.com>
2011-05-01 12:06:56 +0200

    Added error andling function

--- include/AoTK/window_handler.h
+++ include/AoTK/window_handler.h
@@ -22,9 +22,11 @@
 
 #endif
 
-#define GLERRORS() //AoTK::getAllError(__FILE__,__LINE__)
+#define GLERRORS() AoTK::getAllError(__FILE__,__LINE__)
 
 namespace AoTK{
+    void getAllError(std::string file, int line);
+
     enum KEY{
         F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12,F13,F14,F15,F16,F17,F18,F19,F20,F21,F22,F23,F24,
         ESC,
