ff61b2255f1dd5990ae103d9562fca8cc7398a7f
r-englund <rickard@r-englund.com>
2011-05-03 13:06:53 +0200

    Added macros so that if a error type doesnt exist it will not be compiled

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -261,30 +261,47 @@ void getAllError(std::string file, int line){
         switch (err){
             case GL_NO_ERROR:
                 break;
+            #ifdef GL_INVALID_ENUM
             case GL_INVALID_ENUM:
                 std::cerr <<i++ << " GL_INVALID_ENUM @ " << file <<":"<<line << ": An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag." << std::endl;
                 break;
+            #endif
+            #ifdef GL_INVALID_VALUE
             case GL_INVALID_VALUE:
                 std::cerr <<i++  << "GL_INVALID_VALUE @ " << file <<":"<<line  << ": A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag." << std::endl;
                 break;
+            #endif
+            #ifdef GL_INVALID_OPERATION
             case GL_INVALID_OPERATION:
                 std::cerr <<i++  << "GL_INVALID_OPERATION @ " << file <<":"<<line  << ": The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag." << std::endl;
                 break;
+            #endif
+            #ifdef GL_STACK_OVERFLOW
             case GL_STACK_OVERFLOW:
                 std::cerr <<i++  << "GL_STACK_OVERFLOW @ " << file <<":"<<line  << ": This command would cause a stack overflow. The offending command is ignored and has no other side effect than to set the error flag." << std::endl;
                 break;
+            #endif
+            #ifdef GL_STACK_UNDERFLOW
             case GL_STACK_UNDERFLOW:
                 std::cerr <<i++  << "GL_STACK_UNDERFLOW @ " << file <<":"<<line  << ": This command would cause a stack underflow. The offending command is ignored and has no other side effect than to set the error flag." << std::endl;
                 break;
+            #endif
+            #ifdef GL_OUT_OF_MEMORY
             case GL_OUT_OF_MEMORY:
                 std::cerr <<i++  << "GL_OUT_OF_MEMORY @ " << file <<":"<<line  << ": There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded." << std::endl;
                 break;
+            #endif
+            #ifdef GL_TABLE_TOO_LARGE
             case GL_TABLE_TOO_LARGE:
                 std::cerr <<i++  << "GL_TABLE_TOO_LARGE @ " << file <<":"<<line  << ": The specified table exceeds the implementation's maximum supported table size. The offending command is ignored and has no other side effect than to set the error flag." << std::endl;
                 break;
+            #endif
+            #ifdef GL_INVALID_FRAMEBUFFER_OPERATION
             case GL_INVALID_FRAMEBUFFER_OPERATION:
                 std::cerr <<i++  << "GL_INVALID_FRAMEBUFFER_OPERATION @ " << file <<":"<<line  << ": The framebuffer object is not complete. The offending command is ignored and has no other side effect than to set the error flag." << std::endl;
                 break;
+            #endif
+
             default:
                 std::cerr <<i++  << "GL_ERROR @ " << file <<":"<<line  << ": Unkown error:" << err << std::endl;
         }
