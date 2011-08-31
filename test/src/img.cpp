1fe16b394ddbdba643ee88384563952bb1e0fd73
Rickard Englund <rickard@r-englund.com>
2011-08-31 20:33:53 +0200

    starting to add test for screenshots

--- test/src/img.cpp
+++ test/src/img.cpp
@@ -5,8 +5,17 @@
 using namespace AoTK;
 using namespace AoTK::IMG;
 
-int main(int argc,char ** argv){
-	int fails = 0;
+int testScreens(){
+    int fails = 0;
+    auto w = AoTK::Window::createWindow(600,600,"Testing Screens");
+    w->setDisplayFunction([](){});
+    w->redraw();
+    w->start();
+    return fails;
+}
+
+int testRotate(){
+    int fails = 0;
 	
     auto img1 = Image<uint8_t>::random(4,4,1);
     auto img2 = img1.rotate(90).rotate(90).rotate(90).rotate(90);
@@ -19,7 +28,6 @@ int main(int argc,char ** argv){
 
 	fails += my_assert_false(img7 != img1,"auto img7 = img1 directly followed by img7 != img1 evaluates to true");
 	fails += my_assert_true(img7 == img1,"auto img7 = img1 directly followed by img7 == img1 evaluates to false");
-
 	
 	for(int x = 0;x<4;x++)for(int y = 0;y<4;y++){
 		if(img1.getPixel(x,y) != img2.getPixel(x,y)){
@@ -54,5 +62,13 @@ int main(int argc,char ** argv){
 	}
 	
 
+	return fails;
+}
+
+int main(int argc,char ** argv){
+    AoTK::init();
+	int fails = 0;
+	fails += testRotate();
+	fails += testScreens();
 	return fails;
 }
